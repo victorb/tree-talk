9363e680a451b13c7bfce2d5580a1ff623694579
Victor Bjelkholm <victorbjelkholm@gmail.com>
2017-10-09 17:25:18 +0200

    Clear out ejected cra

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const CHANNEL = 'dev-tree-talk'
-export const SIGNAL_SERVER = '/libp2p-webrtc-star/ip4/127.0.0.1/tcp/9090/ws'
-// export const SIGNAL_SERVER = '/libp2p-webrtc-star/dns4/star-signal.cloud.ipfs.team/wss'
+// export const SIGNAL_SERVER = '/libp2p-webrtc-star/ip4/127.0.0.1/tcp/9090/ws'
+export const SIGNAL_SERVER = '/libp2p-webrtc-star/dns4/star-signal.cloud.ipfs.team/wss'
